225de5959cc1502c8c30047ac97c076b5e006bd0
yankewei <yankewei1993@gmail.com>
2025-05-14 21:11:02 +0800

    转义特殊字符

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">I'm Kewei Yan</h1>
+      <h1 className="mb-4 text-2xl font-bold">I&apos;m Kewei Yan</h1>
       <p className="text-fd-muted-foreground">
-        I'm a software engineer, welcome to my personal website.
+        I&apos;m a software engineer, welcome to my personal website.
       </p>
       <p className="text-fd-muted-foreground mt-2">
         You can check out my articles{' '}
